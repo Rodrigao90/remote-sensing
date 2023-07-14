466fc37269ab9227f0f2804894e7377417c697f6
RPMUnicap <rodrigo.paula@unicap.br>
2023-07-14 17:00:53 -0300

    Authentication_file.txt, .gitignore and download.py uptadet.

--- app/download.py
+++ app/download.py
@@ -1,20 +1,21 @@
 from download_utils import *
 from main_utils import *
+from authentication import *
 
 
 ## Define search parameters
 
 reuse_query_information()
-
 r_json = read_request_json()
-
 date_i, date_f, platform_name, product_type, sensor_op_mode = retrieve_information_from_json(r_json)
-
 
 
 ## Authentication
 
-api = authenticate_access()
+api = authenticate_access('authentication_file.txt')
+
+
+## Query
 
 products_df, count = query(api, 
         date_i,
